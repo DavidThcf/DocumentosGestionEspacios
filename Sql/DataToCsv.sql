8c877e6984ccae74a55a3247be72f9502d6fa2a5
David <thcf963@hotmail.com>
2018-05-25 11:39:40 -0500

    11:39 25/05/2018

---
+++ Sql/DataToCsv.sql
@@ -0,0 +1,26 @@
+﻿COPY general.gener_sede TO'D:\datacsv\csv\gener_sede.csv' DELIMITER ';' CSV HEADER;
+
+COPY general.gener_area_construida TO'D:\datacsv\csv\gener_area_construida.csv' DELIMITER ';' CSV HEADER;
+
+COPY general.gener_espacio_fisico TO'D:\datacsv\csv\gener_espacio_fisico.csv' DELIMITER ';'CSV HEADER;
+
+COPY general.gener_tipo_espacio TO'D:\datacsv\csv\gener_tipo_espacio.csv' DELIMITER ';' CSV HEADER;
+
+COPY general.gener_atributos_espacio_fisico  TO'D:\datacsv\csv\gener_atributos_espacio_fisico.csv' DELIMITER ';'CSV HEADER;
+
+COPY planeacion.plan_dia  TO'D:\datacsv\csv\plan_dia.csv' DELIMITER ';' CSV HEADER;
+
+COPY planeacion.plan_horario_aula  TO'D:\datacsv\csv\plan_horario_aula.csv' DELIMITER ';' CSV HEADER;
+
+COPY planeacion.plan_asignacion_aula  TO'D:\datacsv\csv\plan_asignacion_aula.csv' DELIMITER ';'CSV HEADER;
+
+COPY planeacion.plan_solicitante  TO'D:\datacsv\csv\plan_solicitante.csv' DELIMITER ';' CSV HEADER;
+
+COPY planeacion.plan_evento  TO'D:\datacsv\csv\plan_evento.csv' DELIMITER ';' CSV HEADER;
+
+COPY planeacion.plan_solicitud  TO'D:\datacsv\csv\plan_solicitud.csv' DELIMITER ';' CSV HEADER;
+
+COPY planeacion.plan_horario_espacio  TO'D:\datacsv\csv\plan_horario_espacio.csv' DELIMITER ';' CSV HEADER;
+
+COPY planeacion.plan_asignacion_espacio  TO'D:\datacsv\csv\plan_asignacion_espacio.csv' DELIMITER ';' CSV HEADER;
+
