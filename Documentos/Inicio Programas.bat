bdd588230035390af9e499111b09f9a4d4def8fb
David <thcf963@hotmail.com>
2018-05-28 14:48:15 -0500

    Cambio

---
+++ Documentos/Inicio Programas.bat
@@ -0,0 +1,12 @@
+D:
+cd D:\datacsv\Documentos
+start cmd /K InicioCliente.bat
+start cmd /K InicioWS.bat
+
+C:
+
+cd C:\Program Files (x86)\Google\Chrome\Application
+start chrome.exe http://localhost:5006
+
+cd C:\Program Files\Microsoft VS Code
+start Code.exe D:\gestionEspacios
