f6e6d45c52ad6fe70b1780c3f9ca8657d652e12d
David <thcf963@hotmail.com>
2018-08-30 17:50:01 -0500

    auto-30/08/2018-17:50:00,24

--- Backups/DatosRestore.bat
+++ Backups/DatosRestore.bat
@@ -5,13 +5,13 @@ set host=90.1.1.32
 set port=5433
 set user=postgres
 set dbname=ciudenar
-set dir=D:\datacsv\Backups\ciudenarDatos.backup
+set dir=C:\Users\UIT\Desktop\ciudenar1.sql
 
 --psql.exe -h %host% -p %port% -U %user% < %dir%
 --psql.exe --help
 
-pg_restore.exe --host %host% --port %port% --username %postgres% --dbname %ciudenar% --no-password  --verbose "D:\datacsv\Backups\ciudenarDatos.backup"
-psql.exe --help
+pg_restore.exe --host %host% --port %port% --username %postgres% --dbname %ciudenar% --no-password  --verbose "C:\Users\UIT\Desktop\ciudenar1.sql"
+--psql.exe --help
 
 
 PAUSE
